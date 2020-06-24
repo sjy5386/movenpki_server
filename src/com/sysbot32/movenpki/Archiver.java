0d70d1f8087ed229eea816a328c68d371f2f9a7c
Seokjin Yoon <sjy5386@gmail.com>
2020-06-24 20:21:45 +0900

    Add class Archiver

---
+++ src/com/sysbot32/movenpki/Archiver.java
@@ -0,0 +1,86 @@
+package com.sysbot32.movenpki;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class Archiver {
+    public void zip(String input, String output) {
+        String[] files = searchDirectory(input);
+        try {
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(output));
+            for (String file : files) {
+                System.out.println(file);
+                zipOutputStream.putNextEntry(new ZipEntry(file.replace(input + "\\", "")));
+                FileInputStream fileInputStream = new FileInputStream(file);
+                int len = (int) Files.size(Paths.get(file));
+                if (len > 0) {
+                    byte[] buf = new byte[len];
+                    fileInputStream.read(buf, 0, len);
+                    zipOutputStream.write(buf, 0, len);
+                }
+                zipOutputStream.closeEntry();
+                fileInputStream.close();
+            }
+            zipOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unzip(String input, String output) {
+        try {
+            ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(input));
+            ZipEntry zipEntry;
+            while (Objects.nonNull(zipEntry = zipInputStream.getNextEntry())) {
+                String path = output;
+                String zipName = zipEntry.getName();
+                System.out.println(zipName);
+                int index = zipName.lastIndexOf('\\');
+                if (index != -1) {
+                    path += "\\" + zipName.substring(0, index);
+                }
+                if (!Files.isDirectory(Paths.get(path))) {
+                    Files.createDirectories(Paths.get(path));
+                }
+                FileOutputStream fileOutputStream = new FileOutputStream(output + "\\" + zipName);
+                byte[] buf = new byte[4096];
+                int len;
+                while ((len = zipInputStream.read(buf)) > 0) {
+                    fileOutputStream.write(buf, 0, len);
+                }
+                zipInputStream.closeEntry();
+                fileOutputStream.flush();
+                fileOutputStream.close();
+            }
+            zipInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String[] searchDirectory(String path) {
+        ArrayList<String> files = new ArrayList<>();
+        searchingDirectory(new File(path), files);
+        return files.toArray(new String[0]);
+    }
+
+    private void searchingDirectory(File file, List<String> result) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                searchingDirectory(f, result);
+            }
+        } else {
+            result.add(file.getPath());
+        }
+    }
+}

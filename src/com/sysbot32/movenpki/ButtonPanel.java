cf031a09600beb4caafabbd2725d150ab388bfee
Seokjin Yoon <sjy5386@gmail.com>
2020-06-27 20:48:28 +0900

    Add class ButtonPanel

---
+++ src/com/sysbot32/movenpki/ButtonPanel.java
@@ -0,0 +1,29 @@
+package com.sysbot32.movenpki;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ButtonPanel {
+    private JPanel panel;
+    private JButton exportButton;
+    private JButton importButton;
+
+    public ButtonPanel() {
+        panel = new JPanel();
+        exportButton = new JButton("PC → 스마트폰");
+        importButton = new JButton("스마트폰 → PC");
+
+        panel.setLayout(new GridLayout(1, 2));
+        exportButton.setFont(MainFrame.FONT_MALGUN);
+        importButton.setFont(MainFrame.FONT_MALGUN);
+        exportButton.setBackground(MainFrame.COLOR_EXPORT);
+        importButton.setBackground(MainFrame.COLOR_IMPORT);
+
+        panel.add(exportButton);
+        panel.add(importButton);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+}
--- src/com/sysbot32/movenpki/MainFrame.java
+++ src/com/sysbot32/movenpki/MainFrame.java
@@ -2,7 +2,6 @@ package com.sysbot32.movenpki;
 
 import javax.swing.*;
 import java.awt.*;
-import java.nio.ByteBuffer;
 
 public class MainFrame {
     public static final Font FONT_MALGUN = new Font("맑은 고딕", Font.PLAIN, 16);
@@ -11,41 +10,22 @@ public class MainFrame {
 
     private JFrame frame;
     private JPanel contentPane;
+    private ButtonPanel buttonPanel;
     private JLabel addressLabel;
-    private JButton exportButton;
-    private JButton importButton;
 
     public MainFrame() {
         frame = new JFrame("MoveNPKI");
         contentPane = (JPanel) frame.getContentPane();
+        buttonPanel = new ButtonPanel();
         addressLabel = new JLabel(Server.getHostAddress());
-        exportButton = new JButton("PC → 스마트폰");
-        importButton = new JButton("스마트폰 → PC");
 
-        frame.setSize(250, 150);
-        frame.setResizable(false);
+        frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        contentPane.setLayout(new FlowLayout());
+        contentPane.setLayout(new BorderLayout());
         addressLabel.setFont(FONT_MALGUN);
-        exportButton.setFont(FONT_MALGUN);
-        importButton.setFont(FONT_MALGUN);
-        exportButton.setBackground(COLOR_EXPORT);
-        importButton.setBackground(COLOR_IMPORT);
 
-        contentPane.add(addressLabel);
-        contentPane.add(exportButton);
-        contentPane.add(importButton);
-
-        exportButton.addActionListener(e -> {
-            new Archiver().zip("C:\\Program Files\\NPKI", "C:\\MoveNPKI\\NPKI.zip");
-            byte[] npki = new FileManager().read("C:\\MoveNPKI\\NPKI.zip");
-            JOptionPane.showMessageDialog(frame, "스마트폰에서 < PC → 스마트폰 > 버튼을 터치해주세요.");
-        });
-        importButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(frame, "스마트폰에서 < 스마트폰 → PC > 버튼을 터치하고 < OK > 버튼을 클릭하세요.");
-            new FileManager().write("C:\\MoveNPKI\\NPKI.zip", null);
-            new Archiver().unzip("C:\\MoveNPKI\\NPKI.zip", "C:\\Program Files\\NPKI");
-        });
+        contentPane.add(addressLabel, BorderLayout.CENTER);
+        contentPane.add(buttonPanel.getPanel(), BorderLayout.SOUTH);
     }
 
     public JFrame getFrame() {
